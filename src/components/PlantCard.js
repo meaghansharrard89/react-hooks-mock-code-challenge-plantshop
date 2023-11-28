41531c6db3baa36663f508b905031bf790a914b6
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-28 13:58:55 -0800

    passing code

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard( { plant } ) {
+  const { name, image, price } = plant;
+  const [isClicked, setIsClicked] = useState(true);
+
+  function handleClick(){
+    setIsClicked((isClicked) => !isClicked);
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={"plant name"} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
+      {isClicked ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch ('http://localhost:6001/plants')
+    .then((res) => res.json())
+    .then((data) => setPlants(data))
+  }, [])
+
+  function handleNewPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  function handleSearch(plant){
+    setSearch(plant)
+  }
+
+  const filteredPlants = plants.filter((p) => 
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm handleNewPlant={handleNewPlant}/>
+      <Search onSearch={handleSearch}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
